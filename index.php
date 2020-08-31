5aa36158f966134d81992292f89ac54127cd53bc
Jessy The <jessy@brightfish.nl>
2020-08-31 11:15:03 +0200

    Class Attempt but could not make it work so going back

--- index.php
+++ index.php
@@ -5,139 +5,149 @@ require 'vendor/autoload.php';
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
-$spreadsheet = IOFactory::load('1jan.17aug.Allaccounts.xls');
 
 class MoneyApp {
 
-    public fixedCosts = [
-        'SPAREN' => 'Sparen',
-        'DEBETRENTE' => 'Bank',
-        'Ohra' => 'Zorgverzekering',
-        'CZ Groep' => 'Zorgverzekering',
-        'Nieuwburen' => 'Huur woning',
-        'Jumbo' => 'Boodschappen (levensmiddelen)',
-        'JUMBO' => 'Boodschappen (levensmiddelen)',
-        'Albert Heijn' => 'Boodschappen (levensmiddelen)',
-        'ALBERT HEIJN' => 'Boodschappen (levensmiddelen)',
-        'AH to go' => 'Boodschappen (levensmiddelen)',
-        'Lidl' => 'Boodschappen (levensmiddelen)',
-        'OneFit' => 'Sporten (fitness)',
-        'Picnic' => 'Boodschappen (levensmiddelen)',
-        'Spotify' => 'Productiviteit',
-        'BELASTINGDIENST' => 'Belastingdienst',
-        'Groceries' => 'Bunq rekening Groceries',
-        'AKH The' => 'Ouders',
-        'Brightfish' => 'Salaris',
-        'Loogman' => 'Auto wassen',
-        'van verzekering 472571230' => 'Verzekering ABN',
-        'ZIGGO' => 'Internet',
-        'COINBASE' => 'Investeringen',
-        'ANWB' => 'Auto-verzekering',
-        'T-MOBILE NETHERLANDS' => 'Telefoon-abonnement',
-        'PEARLE' => 'Persoonlijke verzorging',
-        'NETFLIX INTERNATIONAL' => 'Activiteiten',
-        'Waternet' => 'Water',
-        'WATERNET' => 'Water',
-        'INNOVA ENERGIE' => 'Energie',
-        'Gemeente' => 'Gemeente Amsterdam',
-        'LTSS 1' => 'Sparen',
-        'BUDGETENERGIE' => 'Energie',
-        'Maandtariferingsnota' => 'Bank',
-        'JESSY Y S THE' => 'Overboeking naar eigen rekening',
-        'J.Y.S. The' => 'Overboeking',
-        'Bunq' => 'Storting naar / van Bunq',
-        'Necessities' => 'Overboeking',
-        'SPORT HOTEL' => 'Sporten (Tennis)',
-        'UNIGARANT' => 'Auto-verzekering',
-        'NL07ABNA0598249389' => 'Overboeking (eigen rekening)',
-        'Jaarpremie' => 'Jaarpremie BANK',
-        'Diana' => 'Huur huisgenoot / terugbetaling',
-        'MICROSOFT' => 'Productiviteit',
-        'Q PARK' => 'Auto (parkeren)',
-        'AUTOBEDRIJF' => 'Auto (langs Maasgroep)',
-    ];
+    public static $spreadsheet;
 
-    public flexibleCosts = [
-        'LINKEDIN' => 'Digitale aankoop',
-        'CODING' => 'Coding mentor',
-        'Mollie' => 'Digitale aankoop',
-        'Tikkie' => 'Tikkie',
-        'Schoenmaker' => 'Huis',
-        'Shilla' => 'Afhaal (eten)',
-        'alloverpiercings' => 'Shoppen',
-        'STORTING' => 'Geld storten',
-        'Zalando' => 'Shoppen',
-        'Coolblue' => 'Shoppen',
-        'Pathe de Munt' => 'Activiteiten',
-        'Deliveroo' => 'Thuisbezorging (eten)',
-        'Eweka' => 'Digitale services',
-        'EWEKA' => 'Digitale services',
-        'Centraal Justitieel Incassobureau' => 'Schuld',
-        'Success Resources' => 'Opleiding (Quantum Leap)',
-        'SUCCESS RESOURCES' => 'Opleiding (Quantum Leap)',
-        'bol.com b.v.' => 'Shoppen',
-        'BOLCOM' => 'Shoppen',
-        'Parkeren' => 'Auto',
-        'Holland & Barrett' => 'Persoonlijke verzorging',
-        'NS' => 'OV',
-        'GVB' => 'OV',
-        'NAT POSTCODE LOTERIJ' => 'Nationale PostCodeLoterij',
-        'Berlin' => 'Reizen',
-        'Ikea' => 'Huis',
-        'Toko Manisan' => 'Afhaal (eten)',
-        'LAKWERK' => 'Activiteiten',
-        'Geldmaat' => 'Pinnen',
-        'Channeling Coaching' => 'Spirituele begeleiding',
-        'ING Bank NV Betaalverzoek' => 'Tikkie',
-        'Vishandel Kroon' => 'Afhaal (eten)',
-        'GGN Mastering Credit' => 'Digitale aankoop',
-        'Costes' => 'Shoppen',
-        'Sauna' => 'Activiteiten',
-        'Fysio' => 'Fysio',
-        'HEMA' => 'Shoppen',
-        'HOXTON HOTEL' => 'Activiteiten',
-        'Scheltema' => 'Shoppen',
-        'STG MOLLIE PAYMENTS' => 'Digitale aankoop',
-        'PayPal' => 'Digitale aankoop',
-        'Tree Full of Scones' => 'Opleiding (Business)',
-        'ASOS' => 'Shoppen',
-        'Worldpay' => 'Shoppen',
-        'KFC' => 'Afhaal (eten)',
-        'TwinSport' => 'Shoppen',
-        'SEOUL FOOD' => 'Afhaal (eten)',
-        'CAFE' => 'Activiteiten',
-        'IJMUIDEN' => 'Activiteiten',
-        'SHEIN.COM' => 'Shoppen',
-        'IKEA' => 'Huis',
-        'Bloomon' => 'Gift',
-        'Milagros mundo' => 'Shoppen',
-        'Hilda' => 'Activiteiten',
-        'Luis Thoolen' => 'Activiteiten',
-        'Montolalu' => 'Activiteiten',
-        'Amely' => 'Activiteiten',
-        'Marlon' => 'Activiteiten',
-        'Kruidvat' => 'Persoonlijke verzorging',
-        'FonQ' => 'Shoppen',
-        'Adyen' => 'Digitale aankoop',
-        'Orientique' => 'Shoppen',
-        'Bakkerij Hulleman' => 'Afhaal (eten)',
-        'HMS Host International' => 'Activiteiten',
-        'Nourished Nederland' => 'Shoppen',
-        'Spiritual Garden' => 'Shoppen',
-        'Facefactory' => 'Persoonlijke verzorging (gezichtsverzorging)',
-        'Anja Luczak' => 'Spiritual begeleiding',
-        'Oriental City' => 'Activiteiten',
-        'BEA NR:' => 'Nog onbekende categorie',
-        'BEA   NR:' => 'Nog onbekende categorie',
-    ];
+    public static $fixedCosts = [
+            'SPAREN' => 'Sparen',
+            'DEBETRENTE' => 'Bank',
+            'Ohra' => 'Zorgverzekering',
+            'CZ Groep' => 'Zorgverzekering',
+            'Nieuwburen' => 'Huur woning',
+            'Jumbo' => 'Boodschappen (levensmiddelen)',
+            'JUMBO' => 'Boodschappen (levensmiddelen)',
+            'Albert Heijn' => 'Boodschappen (levensmiddelen)',
+            'ALBERT HEIJN' => 'Boodschappen (levensmiddelen)',
+            'AH to go' => 'Boodschappen (levensmiddelen)',
+            'Lidl' => 'Boodschappen (levensmiddelen)',
+            'OneFit' => 'Sporten (fitness)',
+            'Picnic' => 'Boodschappen (levensmiddelen)',
+            'Spotify' => 'Productiviteit',
+            'BELASTINGDIENST' => 'Belastingdienst',
+            'Groceries' => 'Bunq rekening Groceries',
+            'AKH The' => 'Ouders',
+            'Brightfish' => 'Salaris',
+            'Loogman' => 'Auto wassen',
+            'van verzekering 472571230' => 'Verzekering ABN',
+            'ZIGGO' => 'Internet',
+            'COINBASE' => 'Investeringen',
+            'ANWB' => 'Auto-verzekering',
+            'T-MOBILE NETHERLANDS' => 'Telefoon-abonnement',
+            'PEARLE' => 'Persoonlijke verzorging',
+            'NETFLIX INTERNATIONAL' => 'Activiteiten',
+            'Waternet' => 'Water',
+            'WATERNET' => 'Water',
+            'INNOVA ENERGIE' => 'Energie',
+            'Gemeente' => 'Gemeente Amsterdam',
+            'LTSS 1' => 'Sparen',
+            'BUDGETENERGIE' => 'Energie',
+            'Maandtariferingsnota' => 'Bank',
+            'JESSY Y S THE' => 'Overboeking naar eigen rekening',
+            'J.Y.S. The' => 'Overboeking',
+            'Bunq' => 'Storting naar / van Bunq',
+            'Necessities' => 'Overboeking',
+            'SPORT HOTEL' => 'Sporten (Tennis)',
+            'UNIGARANT' => 'Auto-verzekering',
+            'NL07ABNA0598249389' => 'Overboeking (eigen rekening)',
+            'Jaarpremie' => 'Jaarpremie BANK',
+            'Diana' => 'Huur huisgenoot / terugbetaling',
+            'MICROSOFT' => 'Productiviteit',
+            'Q PARK' => 'Auto (parkeren)',
+            'AUTOBEDRIJF' => 'Auto (langs Maasgroep)',
+        ];
 
-    public numberActiveRows = $spreadsheet->getActiveSheet()->getHighestRow(); // e.g. 10
+    public static $flexibleCosts = [
+            'LINKEDIN' => 'Digitale aankoop',
+            'CODING' => 'Coding mentor',
+            'Mollie' => 'Digitale aankoop',
+            'Tikkie' => 'Tikkie',
+            'Schoenmaker' => 'Huis',
+            'Shilla' => 'Afhaal (eten)',
+            'alloverpiercings' => 'Shoppen',
+            'STORTING' => 'Geld storten',
+            'Zalando' => 'Shoppen',
+            'Coolblue' => 'Shoppen',
+            'Pathe de Munt' => 'Activiteiten',
+            'Deliveroo' => 'Thuisbezorging (eten)',
+            'Eweka' => 'Digitale services',
+            'EWEKA' => 'Digitale services',
+            'Centraal Justitieel Incassobureau' => 'Schuld',
+            'Success Resources' => 'Opleiding (Quantum Leap)',
+            'SUCCESS RESOURCES' => 'Opleiding (Quantum Leap)',
+            'bol.com b.v.' => 'Shoppen',
+            'BOLCOM' => 'Shoppen',
+            'Parkeren' => 'Auto',
+            'Holland & Barrett' => 'Persoonlijke verzorging',
+            'NS' => 'OV',
+            'GVB' => 'OV',
+            'NAT POSTCODE LOTERIJ' => 'Nationale PostCodeLoterij',
+            'Berlin' => 'Reizen',
+            'Ikea' => 'Huis',
+            'Toko Manisan' => 'Afhaal (eten)',
+            'LAKWERK' => 'Activiteiten',
+            'Geldmaat' => 'Pinnen',
+            'Channeling Coaching' => 'Spirituele begeleiding',
+            'ING Bank NV Betaalverzoek' => 'Tikkie',
+            'Vishandel Kroon' => 'Afhaal (eten)',
+            'GGN Mastering Credit' => 'Digitale aankoop',
+            'Costes' => 'Shoppen',
+            'Sauna' => 'Activiteiten',
+            'Fysio' => 'Fysio',
+            'HEMA' => 'Shoppen',
+            'HOXTON HOTEL' => 'Activiteiten',
+            'Scheltema' => 'Shoppen',
+            'STG MOLLIE PAYMENTS' => 'Digitale aankoop',
+            'PayPal' => 'Digitale aankoop',
+            'Tree Full of Scones' => 'Opleiding (Business)',
+            'ASOS' => 'Shoppen',
+            'Worldpay' => 'Shoppen',
+            'KFC' => 'Afhaal (eten)',
+            'TwinSport' => 'Shoppen',
+            'SEOUL FOOD' => 'Afhaal (eten)',
+            'CAFE' => 'Activiteiten',
+            'IJMUIDEN' => 'Activiteiten',
+            'SHEIN.COM' => 'Shoppen',
+            'IKEA' => 'Huis',
+            'Bloomon' => 'Gift',
+            'Milagros mundo' => 'Shoppen',
+            'Hilda' => 'Activiteiten',
+            'Luis Thoolen' => 'Activiteiten',
+            'Montolalu' => 'Activiteiten',
+            'Amely' => 'Activiteiten',
+            'Marlon' => 'Activiteiten',
+            'Kruidvat' => 'Persoonlijke verzorging',
+            'FonQ' => 'Shoppen',
+            'Adyen' => 'Digitale aankoop',
+            'Orientique' => 'Shoppen',
+            'Bakkerij Hulleman' => 'Afhaal (eten)',
+            'HMS Host International' => 'Activiteiten',
+            'Nourished Nederland' => 'Shoppen',
+            'Spiritual Garden' => 'Shoppen',
+            'Facefactory' => 'Persoonlijke verzorging (gezichtsverzorging)',
+            'Anja Luczak' => 'Spiritual begeleiding',
+            'Oriental City' => 'Activiteiten',
+            'BEA NR:' => 'Nog onbekende categorie',
+            'BEA   NR:' => 'Nog onbekende categorie',
+        ];
 
-    makingSpreadSheet();
+    public static $numberActiveRows;
 
-    public function makingSpreadSheet() {
+    public function __construct($fixedCosts, $flexibleCosts, $spreadsheet) {
 
-        for($i = 2; $i <= $this->$numberActiveRows; $i++) {
+        $this->spreadsheet = IOFactory::load('1jan.17aug.Allaccounts.xls');
+        $this->fixedCosts = $fixedCosts;
+        $this->flexibleCosts = $flexibleCosts;
+        $this->numberActiveRows = $spreadsheet->getActiveSheet()->getHighestRow();
+
+        $this->makingSpreadSheet('hellokonnichiwa');
+
+    }
+
+    public function makingSpreadSheet(string $englishJapanese) {
+
+        for($i = 2; $i <= $this->numberActiveRows; $i++) {
 
             $dateNotation = strtotime($spreadsheet->getActiveSheet()->getCellByColumnAndRow(2, $i)->getCalculatedValue());
             $date = date('d F Y', $dateNotation);
@@ -155,9 +165,9 @@ class MoneyApp {
 
         }
 
-        foreach($this->$fixedCosts as $target => $category) {
+        foreach($this->fixedCosts as $target => $category) {
 
-            for($i = 2; $i <= $numberActiveRows; $i++) {
+            for($i = 2; $i <= $this->numberActiveRows; $i++) {
 
                 $description = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(4, $i)->getCalculatedValue();
 
@@ -168,9 +178,9 @@ class MoneyApp {
             }
         }
 
-        foreach($flexibleCosts as $target => $category) {
+        foreach($this->flexibleCosts as $target => $category) {
 
-            for($i = 2; $i <= $numberActiveRows; $i++) {
+            for($i = 2; $i <= $this->numberActiveRows; $i++) {
 
                 $description = $spreadsheet->getActiveSheet()->getCellByColumnAndRow(4, $i)->getCalculatedValue();
 
@@ -187,19 +197,23 @@ class MoneyApp {
     $writer->save($nameSheet . '.xls');
 
     $newSpreadsheet = IOFactory::load('categoriesAdded.xls');
+    $this->makeParsedData($newSpreadsheet, 'konnichiwa');
 
-    makeParsedData($newSpreadsheet);
+
     }
+
 
     /////////////////////////////////////////////////////////////////////////
                                 // parsedData met rowObjects maken
     ////////////////////////////////////////////////////////////////////////
 
-    function makeParsedData(IOFactory $newSpreadsheet) {
+    public function makeParsedData($newSpreadsheet, string $greeting) {
+
+        echo $newSpreadsheet;
 
         $parsedData = [];
 
-        for($i = 2; $i <= $numberActiveRows; $i++) {
+        for($i = 2; $i <= $this->numberActiveRows; $i++) {
 
             $row = 'Row' . $i;
 
@@ -251,7 +265,7 @@ class MoneyApp {
                 }
         });
 
-        monthFiltering($results49, '498586642');
+        $this->monthFiltering($results49, '498586642');
     }
 
     public function monthFiltering(array $resultsAccount, string $accountNumber) {
@@ -270,15 +284,15 @@ class MoneyApp {
                     $results[$accountNumber][$month][] = $row;
                 }
             }
-            monthCalculation($results[$accountNumber][$month], $month);
+            $this->monthCalculation($results[$accountNumber][$month], $month);
         }
     }
 
-    public function monthCalculation(array $monthlyResults, string $month) {
+    function monthCalculation(array $monthlyResults, string $month) {
 
         $month = [];
 
-        $categoriesCosts = array_keys($this->$fixedCosts);
+        $categoriesCosts = array_keys($this->fixedCosts);
 
         foreach($monthlyResults as $row) {
 
